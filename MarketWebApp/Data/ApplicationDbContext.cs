7e280104f8aa7780d9e6cf71186f53585a0fdf2d
FarghlyNageh <FarghlyNageh@DESKTOP-QD7RJ65>
2024-03-17 03:12:44 +0200

    productAdd project files.

---
+++ MarketWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MarketWebApp.Models.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet properties and other configurations...
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public virtual DbSet<OrderProduct> OrderProduct { get; set; }
+    }
+}
---
+++ MarketWebApp/Models/Entity/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketWebApp.Models.Entity
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Img { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public int Price { get; set; }
+
+    }
+}
+
